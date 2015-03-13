d5ba94afc5209dcdd6fc8898a246cf65a9086bc5
Sung Won Cho <mikeswcho@gmail.com>
2015-03-13 22:14:02 +1100

    Implement Distance

--- lib/k_means_ruby/space.rb
+++ lib/k_means_ruby/space.rb
@@ -1,7 +1,9 @@
 module KMeansRuby
   class Space
     class << self
-
+      def get_distance(p1, p2)
+        Math.sqrt( (p1.x - p2.x)**2 + (p1.y - p2.y)**2 )
+      end
     end
   end
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 describe KMeansRuby::Space do
-  
+
+  let(:p1) { KMeansRuby::Point.new(1,2) }
+  let(:p2) { KMeansRuby::Point.new(5,5) }
+
+  describe '.get_distance' do
+    it 'returns the distance of two points' do
+      distance = KMeansRuby::Space.get_distance(p1, p2)
+
+      expect(distance).to eq 5
+    end
+  end
 end
