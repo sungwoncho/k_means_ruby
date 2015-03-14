b8f5a00554cd9d06da5f6c4fa762f5496fb2601f
Sung Won Cho <mikeswcho@gmail.com>
2015-03-14 13:14:18 +1100

    Fix file loading

--- lib/k_means_ruby.rb
+++ lib/k_means_ruby.rb
@@ -1,5 +1,8 @@
+$LOAD_PATH.unshift(File.expand_path('../k_means_ruby',__FILE__))
+
 module KMeansRuby
   autoload :Cluster, 'cluster'
   autoload :Point, 'point'
   autoload :Space, 'space'
+  autoload :Algorithm, 'algorithm'
 end
